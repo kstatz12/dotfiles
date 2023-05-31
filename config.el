bde42a6d456799d34e6ff81d3853352be8a716b4
Karl <karl.statz@gmail.com>
2023-05-31 00:07:54 -0500

    Update config.el

--- config.el
+++ config.el
@@ -1,79 +1,12 @@
-;;; $DOOMDIR/config.el -*- lexical-binding: t; -*-
+(setq user-full-name "Karl Statz"
+      user-mail-address "karl.statz@gmail.com")
 
-;; Place your private configuration here! Remember, you do not need to run 'doom
-;; sync' after modifying this file!
+(setq doom-theme 'doom-homage-black)
+(setq doom-font (font-spec :family "Fira Code" :size 14 :weight 'medium))
 
 
-;; Some functionality uses this to identify you, e.g. GPG configuration, email
-;; clients, file templates and snippets. It is optional.
-(setq user-full-name "John Doe"
-      user-mail-address "john@doe.com")
-
-;; Doom exposes five (optional) variables for controlling fonts in Doom:
-;;
-;; - `doom-font' -- the primary font to use
-;; - `doom-variable-pitch-font' -- a non-monospace font (where applicable)
-;; - `doom-big-font' -- used for `doom-big-font-mode'; use this for
-;;   presentations or streaming.
-;; - `doom-unicode-font' -- for unicode glyphs
-;; - `doom-serif-font' -- for the `fixed-pitch-serif' face
-;;
-;; See 'C-h v doom-font' for documentation and more examples of what they
-;; accept. For example:
-;;
-;;(setq doom-font (font-spec :family "Fira Code" :size 12 :weight 'semi-light)
-;;      doom-variable-pitch-font (font-spec :family "Fira Sans" :size 13))
-;;
-;; If you or Emacs can't find your font, use 'M-x describe-font' to look them
-;; up, `M-x eval-region' to execute elisp code, and 'M-x doom/reload-font' to
-;; refresh your font settings. If Emacs still can't find your font, it likely
-;; wasn't installed correctly. Font issues are rarely Doom issues!
-
-;; There are two ways to load a theme. Both assume the theme is installed and
-;; available. You can either set `doom-theme' or manually load a theme with the
-;; `load-theme' function. This is the default:
-(setq doom-theme 'manoj-dark)
-
-;; This determines the style of line numbers in effect. If set to `nil', line
-;; numbers are disabled. For relative line numbers, set this to `relative'.
 (setq display-line-numbers-type t)
 
-;; If you use `org' and don't want your org files in the default location below,
-;; change `org-directory'. It must be set before org loads!
-(setq org-directory "~/org/")
-
-
-;; Whenever you reconfigure a package, make sure to wrap your config in an
-;; `after!' block, otherwise Doom's defaults may override your settings. E.g.
-;;
-;;   (after! PACKAGE
-;;     (setq x y))
-;;
-;; The exceptions to this rule:
-;;
-;;   - Setting file/directory variables (like `org-directory')
-;;   - Setting variables which explicitly tell you to set them before their
-;;     package is loaded (see 'C-h v VARIABLE' to look up their documentation).
-;;   - Setting doom variables (which start with 'doom-' or '+').
-;;
-;; Here are some additional functions/macros that will help you configure Doom.
-;;
-;; - `load!' for loading external *.el files relative to this one
-;; - `use-package!' for configuring packages
-;; - `after!' for running code after a package has loaded
-;; - `add-load-path!' for adding directories to the `load-path', relative to
-;;   this file. Emacs searches the `load-path' when you load packages with
-;;   `require' or `use-package'.
-;; - `map!' for binding new keys
-;;
-;; To get information about any of these functions/macros, move the cursor over
-;; the highlighted symbol at press 'K' (non-evil users must press 'C-c c k').
-;; This will open documentation for it, including demos of how they are used.
-;; Alternatively, use `C-h o' to look up a symbol (functions, variables, faces,
-;; etc).
-;;
-;; You can also try 'gd' (or 'C-c c d') to jump to their definition and see how
-;; they are implemented.
 (setq org-directory "~/Dropbox/org/")
 
 (after! lsp-mode (dolist (dir '(
@@ -89,13 +22,30 @@
 (after! lsp-mode (setq lsp-enable-file-watchers `t))
 
 (after! org (setq org-agenda-files `("~/Dropbox/org/")))
+
 (after! org (setq org-capture-templates
-        `(("t" "Todo" entry
-           (file+headline "~/Dropbox/org/todo.org" "Tasks")
-           "* TODO %?\n Created: %u\n")
+        '(
+          ("t" "Todo" entry (file+headline "~/Dropbox/org/todo.org" "Tasks")
+           "* TODO %?\n Created: %u\n");
+          ("n" "Notes" entry (file "~/Dropbox/org/notes.org")
+           "* %?\n %F\n");
           )))
 
 (setq sqlformat-command 'pgformatter)
 (setq sqlformat-args '("-s2" "-g" "-w80" "-B"))
 
-(setq org-plantuml-jar-path "~/plantuml-1.2022.8.jar")
+
+(use-package! copilot
+  :hook (prog-mode . copilot-mode)
+  :bind (("C-TAB" . 'copilot-accept-completion-by-word)
+         ("C-<tab>" . 'copilot-accept-completion-by-word)
+         :map copilot-completion-map
+         ("<tab>" . 'copilot-accept-completion)
+         ("TAB" . 'copilot-accept-completion)))
+
+(setq mastodon-instance-url "https://discuss.systems"
+      mastodon-active-user "kstatz12")
+
+(setq org-plantuml-jar-path "~/plantuml-1.2023.0.jar")
+(setq plantuml-jar-path "~/plantuml-1.2023.0.jar")
+
